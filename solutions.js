765dd8382817ac9ab80d68d13b9263774c3b9596
Danielle Tobler <danielle.tobler210@gmail.com>
2020-02-09 21:32:34 -0600

    Completed q7

--- solutions.js
+++ solutions.js
@@ -113,3 +113,16 @@ function isEven(input) {
 function isIdentical(input1, input2) {
     return input1 === input2;
 }
+
+// 7. Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+//
+//  isEqual(3, "3")                       // true
+//  isEqual("abc123", "abc123")           // true
+//  isEqual(true, 1)                      // true
+//  isEqual(0, false)                     // true
+//  isEqual(4, -5)                        // false
+//  isEqual("java", "javascript")         // false
+
+function isEqual(input1, input2) {
+    return input1 == input2;
+}
