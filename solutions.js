11d7d965ed02dab12d9f5c97d52bc57a62a199c6
Danielle Tobler <danielle.tobler210@gmail.com>
2020-02-09 21:22:48 -0600

    Completed problems 1 and 2

--- solutions.js
+++ solutions.js
@@ -20,3 +20,34 @@
  * and
  * concat
  */
+
+
+// 1. Define a function named isTrue that takes in any input and returns true if the input provided is exactly equal to true in value and data type.
+//
+// isTrue(true)             // true
+// isTrue(false)            // false
+// isTrue(0)                // false
+// isTrue(null)             // false
+// isTrue("true")           // false
+// isTrue("Banana")         // false
+// isTrue([1, 2])           // false
+
+function isTrue(input) {
+    return input === true;
+}
+
+// 2. Define a function named isFalse that takes in a value and returns a true if and only if the provided input is equal to false in both type and value.
+//
+// isFalse(false)             // true
+// isFalse(true)              // false
+// isFalse(0)                 // false
+// isFalse(null)              // false
+// isFalse("")                // false
+// isFalse("Banana")          // false
+// isFalse([1, 2])            // false
+
+function isFalse(value) {
+    return value === false;
+}
+
+
